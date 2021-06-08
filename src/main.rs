359e36feebadc51f1186baa2311f151ff4eae6af
Matthew J Perez <matt@mperez.io>
2021-06-07 23:32:12 -0500

    RSS testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+
+use rss::Channel;
+
+async fn get_rss(url: &str) -> Result<Channel, Box<dyn std::error::Error>> {
+    let content = reqwest::get(url).await?.bytes().await?;
+    let channel = Channel::read_from(&content[..])?;
+    Ok(channel)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let path = Path::new("configs/urls.txt");
+    let input = File::open(path)?;
+    let buffered = BufReader::new(input);
+
+    for line in buffered.lines() {
+        let url = line?.to_string();
+
+        let channel = get_rss(&url).await?;
+
+        for item in channel.into_items() {
+            println!(
+                "\nTitle: {}\nDescription: {}\n",
+                item.title().unwrap(),
+                item.description().unwrap()
+            );
+            println!();
+        }
+    }
+
+    Ok(())
 }
